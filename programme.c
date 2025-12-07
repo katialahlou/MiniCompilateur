92f89ba4872a4ee640f9cfc800d592776c90c716
katialahlou <kat.lah1234567890@gmail.com>
2025-12-07 18:55:26 +0100

    Add files via upload

---
+++ programme.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x = 2;
+
+    switch (x {
+        case 1
+            printf("One\n");
+            break;
+        case 2
+            printf("Two\n");
+            break
+        default
+            printf("Other\n");
+    }
+
+    return 0;
+}
+
